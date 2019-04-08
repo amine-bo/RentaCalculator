c4dd3a076d1889cd04f4662a1a56c62e71f94bb6
amine-bo <ronaldio10@hotmail.com>
2019-04-08 22:09:09 +0200

    Splash screen added.

--- app/src/main/java/com/general/rentacalculator/activities/SplashScreenActivity.java
+++ app/src/main/java/com/general/rentacalculator/activities/SplashScreenActivity.java
@@ -1,22 +1,49 @@
 package com.general.rentacalculator.activities;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.general.rentacalculator.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        // Duración en milisegundos del mantenimiento de la splash screen
+        private static final long SPLASH_SCREEN_RETARDO = 5000;
 
-        Intent intent = new Intent(getApplicationContext(),
-                MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Poner orientación vertical
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+            // Esconder la barra de título
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+            setContentView(R.layout.activity_splash_screen);
+
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+
+                    // Iniciar la activity de menu
+                    Intent menuIntent = new Intent().setClass(
+                            SplashScreenActivity.this, MainActivity.class);
+                    startActivity(menuIntent);
+
+                    // Cerrar la actividad para que el usuario no pueda volver pulsando el botón atrás
+                    finish();
+                }
+            };
+
+            // Establecimiento del timer para retrasar la actividad 5 segundos
+            Timer timer = new Timer();
+            timer.schedule(task, SPLASH_SCREEN_RETARDO);
+        }
 }
