cefd408838a65b32653f3e05817fe48ce1920c9c
jjoubair <jad.joubair@soprasteria.com>
2019-04-16 22:22:50 +0200

    Adding  fourth Form with validation and result layout

--- app/src/main/java/com/general/rentacalculator/activities/SecondFormActivity.java
+++ app/src/main/java/com/general/rentacalculator/activities/SecondFormActivity.java
@@ -1,11 +1,9 @@
 package com.general.rentacalculator.activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.general.rentacalculator.R;
