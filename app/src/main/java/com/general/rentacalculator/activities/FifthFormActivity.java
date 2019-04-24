ac0f37af404575d1538861e60ad649b11867b141
amine-bo <ronaldio10@hotmail.com>
2019-04-24 22:18:36 +0200

    - Initialize ResultService and ResultRenta in final activity.

--- app/src/main/java/com/general/rentacalculator/activities/FifthFormActivity.java
+++ app/src/main/java/com/general/rentacalculator/activities/FifthFormActivity.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 
 import com.general.rentacalculator.R;
 import com.general.rentacalculator.model.Renta;
+import com.general.rentacalculator.model.ResultRenta;
+import com.general.rentacalculator.services.ResultService;
 
 public class FifthFormActivity extends AppCompatActivity {
+    private Renta renta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        renta = (Renta) getIntent().getSerializableExtra("rentaModel");
+        ResultRenta resultRenta = new ResultRenta();
+        ResultService resultService = new ResultService();
+        rentaResultCalculator(resultRenta, resultService);
+
+    }
+
+    private void rentaResultCalculator(ResultRenta resultRenta, ResultService resultService) {
+        double base = resultService.calculateBase(renta.getSalarioBruto(), renta.getCotizado(), false, 0);
+        resultRenta.setBase(base);
     }
 }
