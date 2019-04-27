7c1894e8c2ceacb8c2a8365c6457cbb091ce9af3
clema <cristian_loios@hotmail.com>
2019-04-28 00:04:23 +0200

    adding more calculous

--- app/src/main/java/com/general/rentacalculator/services/ResultService.java
+++ app/src/main/java/com/general/rentacalculator/services/ResultService.java
@@ -21,27 +21,65 @@ public class ResultService {
     private final static String DONA_0 = "general.constantes.dona0";
     private final static String DONA_3 = "general.constantes.dona3";
 
-    public void rentaResultCalculator(ResultRenta resultRenta, Renta renta, Context context) {
+    public void rentaResultCalculator(ResultRenta resultRenta, Renta renta, Context context) throws MissingMandatoryValuesException {
         Map<Double, Double> auxCuotaMap = new HashMap<>();
+        // basis
         double base = this.calculateBase(renta.getSalarioBruto(), renta.getCotizado(), false, 0);
         resultRenta.setBase(base);
-        String[] estadoArray = ConfigurationHolder.getProperty(ESTADO, context).split(";");
-        for(String cuota:estadoArray){
-            auxCuotaMap.put(Double.valueOf(cuota.split(",")[0]), Double.valueOf(cuota.split(",")[1]));
-        }
-        double cuotaEstado = this.calculateCota(resultRenta.getBase(),auxCuotaMap) - this.calculateCota(renta.getGravamen(), auxCuotaMap);
+
+        // gravamen
+        renta.setGravamen(calculateGravamen(renta.getEdad(), renta.getDiscapacidad(), renta.isAyuda(), renta.getHijosMenores25Anos(), renta.getHijosMenores3Anos(), renta.getAscendientesMayores65Anos(), renta.getAscendientesMayores75Anos()));
+
+        // state cuota
+        auxCuotaMap = obtainPropertyMap(ESTADO, context);
+        double cuotaEstado = this.calculateCota(resultRenta.getBase(), auxCuotaMap) - this.calculateCota(renta.getGravamen(), auxCuotaMap);
         resultRenta.setCuotaEstado(cuotaEstado);
-        String[] autoArray = ConfigurationHolder.getProperty(AUTO, context).split(";");
-        auxCuotaMap = new HashMap<>();
-        for(String cuota:autoArray){
-            auxCuotaMap.put(Double.valueOf(cuota.split(",")[0]), Double.valueOf(cuota.split(",")[1]));
-        }
-        double cuotaAuto = this.calculateCota(resultRenta.getBase(),auxCuotaMap) - this.calculateCota(renta.getGravamen(), auxCuotaMap);
+
+        // autonomic cuota
+        auxCuotaMap = obtainPropertyMap(AUTO, context);
+        double cuotaAuto = this.calculateCota(resultRenta.getBase(), auxCuotaMap) - this.calculateCota(renta.getGravamen(), auxCuotaMap);
         resultRenta.setCuotaEstado(cuotaAuto);
-        double tipoEst = resultRenta.getCuotaEstado()/resultRenta.getBase() * 100;
-        double tipoAuto = resultRenta.getCuotaAuto()/resultRenta.getBase() * 100;
+        double tipoEst = resultRenta.getCuotaEstado() / resultRenta.getBase() * 100;
+        double tipoAuto = resultRenta.getCuotaAuto() / resultRenta.getBase() * 100;
+        // FIXME interests arent tipo. Actually, i think tipo isnt necessary
         resultRenta.setInteresesEstado(tipoEst);
         resultRenta.setInteresesAuto(tipoAuto);
+
+        // donations
+        if(renta.getDonaciones()>0){
+            Map<Double, Double> tramosDonacion = obtainTramosDonacion(renta.isMas3Anos(), context);
+            double desgravacionByDonation = this.calculateCota(resultRenta.getBase(), tramosDonacion);
+            resultRenta.setDeduccionDonacionFinal(calculateDonationFinal(desgravacionByDonation, resultRenta.getBase()));
+        }
+
+        // state reliefs and taxes
+        double deduccionesEstado = calculateDeduccionesPartial(renta.getDeduccionesEstado(), resultRenta.getDeduccionDonacionFinal());
+        double liquidoEstado = calculateLiquidoPartial(resultRenta.getCuotaEstado(), resultRenta.getInteresesEstado(), deduccionesEstado);
+
+        // autonomic reliefs and taxes
+        double deduccionesAutonomia = calculateDeduccionesPartial(renta.getDeduccionesComunidad(), resultRenta.getDeduccionDonacionFinal());
+        double liquidoAutonomia = calculateLiquidoPartial(resultRenta.getCuotaAuto(), resultRenta.getInteresesAuto(), deduccionesAutonomia);
+
+        // total reliefs and taxes
+        resultRenta.setCuotaLiquida(calculateLiquidoTotal(liquidoEstado, liquidoAutonomia));
+        resultRenta.setRetenidoTotal(calculateRetenidoTotal(renta.getRetenido(), renta.getInteresesRetenidos()));
+
+        // result
+        resultRenta.setResultado(calculateResultado(resultRenta.getCuotaLiquida(), resultRenta.getRetenidoTotal()));
+        resultRenta.setTasaEfectiva(calculateTasaEfectiva(resultRenta.getResultado(), resultRenta.getBase()));
+    }
+
+    private Map<Double, Double> obtainPropertyMap(String property, Context context) {
+        String[] rawData = ConfigurationHolder.getProperty(property, context).split(";");
+        Map<Double, Double> tramos = new HashMap<>();
+        for (String datum : rawData) {
+            tramos.put(Double.valueOf(datum.split(",")[0]), Double.valueOf(datum.split(",")[1]));
+        }
+        return tramos;
+    }
+
+    private Map<Double, Double> obtainTramosDonacion(boolean moreThreeYearsDonating, Context context) {
+        return obtainPropertyMap(moreThreeYearsDonating ? DONA_3 : DONA_0, context);
     }
 
     /**
@@ -90,10 +128,10 @@ public class ResultService {
      * @throws MissingMandatoryValuesException
      */
     private double calculateCotizado(double valorImpositivoDebidoTipoContrato,
-                                    double valorImpositivoContingenciasComunes,
-                                    double valorImpositivoFormacion, double salarioBruto, double exentos,
-                                    int horasExtraOrdinarias, double valorCotizacionHorasExtraOrdinarias,
-                                    int horasExtraFuerza, double valorCotizacionHorasExtraFuerza) throws MissingMandatoryValuesException {
+                                     double valorImpositivoContingenciasComunes,
+                                     double valorImpositivoFormacion, double salarioBruto, double exentos,
+                                     int horasExtraOrdinarias, double valorCotizacionHorasExtraOrdinarias,
+                                     int horasExtraFuerza, double valorCotizacionHorasExtraFuerza) throws MissingMandatoryValuesException {
         double cotizacion = valorImpositivoDebidoTipoContrato + valorImpositivoContingenciasComunes
                 + valorImpositivoFormacion;
         return this.calculateCotizado(cotizacion, salarioBruto, exentos, horasExtraOrdinarias,
@@ -114,8 +152,8 @@ public class ResultService {
      * @param valorCotizacionHorasExtraFuerza     as percentage mandatory if horasExtraFuerza present or else throws MissingMandatoryValuesException
      */
     private double calculateCotizado(double cotizacion, double salarioBruto, double exentos,
-                                    int horasExtraOrdinarias, double valorCotizacionHorasExtraOrdinarias,
-                                    int horasExtraFuerza, double valorCotizacionHorasExtraFuerza) throws MissingMandatoryValuesException {
+                                     int horasExtraOrdinarias, double valorCotizacionHorasExtraOrdinarias,
+                                     int horasExtraFuerza, double valorCotizacionHorasExtraFuerza) throws MissingMandatoryValuesException {
         if (valorCotizacionHorasExtraOrdinarias == 0 && horasExtraOrdinarias > 0) {
             throw new MissingMandatoryValuesException("Regular overtime setted but contribution value is 0");
         } else if (valorCotizacionHorasExtraFuerza == 0 && horasExtraFuerza > 0) {
@@ -199,6 +237,7 @@ public class ResultService {
 
     /**
      * Calculates gravamen
+     *
      * @param contributorAge
      * @param contributorDisabiliy
      * @param requiresHelpOrHaveReducedMobility
@@ -209,9 +248,9 @@ public class ResultService {
      * @return
      * @throws MissingMandatoryValuesException
      */
-    public double calculateGravamen(int contributorAge, DisabilityEnum contributorDisabiliy, boolean requiresHelpOrHaveReducedMobility, int descendentsQuantity, int minors3yo, int ascendentsOlder65OrDisabledQuantity, int ascendentsOlder75Quantity) throws MissingMandatoryValuesException{
+    public double calculateGravamen(int contributorAge, DisabilityEnum contributorDisabiliy, boolean requiresHelpOrHaveReducedMobility, int descendentsQuantity, int minors3yo, int ascendentsOlder65OrDisabledQuantity, int ascendentsOlder75Quantity) throws MissingMandatoryValuesException {
         // mandatory vars
-        if( contributorAge <=0){
+        if (contributorAge <= 0) {
             throw new MissingMandatoryValuesException("Contributor age is missing or invalid");
         }
         // personal situation
@@ -280,88 +319,108 @@ public class ResultService {
 
     /**
      * Calculates actual tax in percentage
+     *
      * @param resultado
      * @param base
      * @return
      * @throws MissingMandatoryValuesException if basis is invalid
      */
-    private double calculateTasaEfectiva(double resultado, double base) throws MissingMandatoryValuesException{
-        if(base <= 0){
+    private double calculateTasaEfectiva(double resultado, double base) throws MissingMandatoryValuesException {
+        if (base <= 0) {
             throw new MissingMandatoryValuesException("Calculus basis is equals to 0 or invalid value");
         }
 
-        return resultado/base;
+        return resultado / base;
     }
 
     /**
      * Calculates result of declaration, whether user has paid in excess or not, therefore, if user
      * has to be returned or has to pay.
      * If result is positive, user has to pay, if it's negative, user has to be paid.
-     * @param cuotaLiquida what actually have to be paid
+     *
+     * @param cuotaLiquida  what actually have to be paid
      * @param retenidoTotal what have been paid
      * @return result
      * @throws MissingMandatoryValuesException if any of both parameters has invalid value
      */
-    private double calculateResultado(double cuotaLiquida, double retenidoTotal) throws MissingMandatoryValuesException{
-        if(cuotaLiquida <= 0){
+    private double calculateResultado(double cuotaLiquida, double retenidoTotal) throws MissingMandatoryValuesException {
+        if (cuotaLiquida <= 0) {
             throw new MissingMandatoryValuesException("Liquid cuota has invalid value");
-        }else if(retenidoTotal <=0){
+        } else if (retenidoTotal <= 0) {
             throw new MissingMandatoryValuesException("Total retained has invalid value");
         }
 
-return cuotaLiquida - retenidoTotal;
+        return cuotaLiquida - retenidoTotal;
     }
 
     /**
      * Calculates actual amount paid by user. It sums up retained money by salary and by interests
+     *
      * @param retenido
      * @param interesesRetenidos
      * @return
      */
-    private double calculateRetenidoTotal(double retenido, double interesesRetenidos){
+    private double calculateRetenidoTotal(double retenido, double interesesRetenidos) {
         return retenido + interesesRetenidos;
     }
 
     /**
      * Calculates total amount user has to pay, summing up what has to pay to state and to autonomic government
+     *
      * @param liquidoEstado
      * @param liquidoAutonomia
      * @return
      */
-    private double calculateLiquidoTotal(double liquidoEstado, double liquidoAutonomia){
+    private double calculateLiquidoTotal(double liquidoEstado, double liquidoAutonomia) {
         return liquidoAutonomia + liquidoEstado;
     }
 
     /**
      * Calculates total to be paid to autonomica government or state
-     * @param cuota from autonomic gov or state
-     * @param intereses from autonomic gov or state
+     *
+     * @param cuota       from autonomic gov or state
+     * @param intereses   from autonomic gov or state
      * @param deducciones from autonomic gov or state
      * @return
      */
-    private double calculateLiquidoPartial(double cuota, double intereses, double deducciones){
+    private double calculateLiquidoPartial(double cuota, double intereses, double deducciones) {
         return cuota + intereses - deducciones;
     }
 
     /**
      * Calculates total reliefs to autonomic government or state
-     * @param deducciones from autonomic gov or state
+     *
+     * @param deducciones          from autonomic gov or state
      * @param desgravacionDonacion total relief by donations
      * @return
      */
-    private double calculateDeduccionesPartial(double deducciones, double desgravacionDonacion){
-        return deducciones + desgravacionDonacion/2;
+    private double calculateDeduccionesPartial(double deducciones, double desgravacionDonacion) {
+        return deducciones + desgravacionDonacion / 2;
     }
 
     /**
      * Calculates amount user has actually paid. Difference would be what State paid to that ONG
-     * @param donacion donation made by user directly to ONG
+     *
+     * @param donacion                   donation made by user directly to ONG
      * @param desgravacioneFinalDonacion reliefed by donation
      * @return
      */
-    private double calculateDonacionEfectiva(double donacion, double desgravacioneFinalDonacion){
+    private double calculateDonacionEfectiva(double donacion, double desgravacioneFinalDonacion) {
         return donacion - desgravacioneFinalDonacion;
-}
+    }
 
+    /**
+     * Calculates actual relief by donation, it cannot be bigger than 10% basis
+     * @param desgravacionByDonation
+     * @param basis
+     * @return
+     */
+    private double calculateDonationFinal(double desgravacionByDonation, double basis) {
+        if (desgravacionByDonation < 0.1 * basis) {
+            return desgravacionByDonation;
+        } else {
+            return 0.1 * basis;
+        }
+    }
 
 }
