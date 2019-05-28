fbd816bbfcf2503a458cafff782d62af617d5cbf
amine-bo <ronaldio10@hotmail.com>
2019-05-28 20:46:37 +0200

    - Info message changed from Tooltip to Toast.

--- app/src/main/java/com/general/rentacalculator/services/RentaUtils.java
+++ app/src/main/java/com/general/rentacalculator/services/RentaUtils.java
@@ -4,24 +4,21 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.general.rentacalculator.R;
 import com.tooltip.Tooltip;
 
 public class RentaUtils {
 
-    public static void setTooltipText(String keyProperties, View view, Context context) {
+    public static void setTooltipText(String keyProperties, View view, final Context context) {
         final String text = ConfigurationHolder.getProperty(keyProperties, context);
         view.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                Tooltip.Builder builder = new Tooltip.Builder(v, R.style.Tooltip2)
-                        .setCancelable(true)
-                        .setDismissOnClick(false)
-                        .setCornerRadius(20f)
-                        .setGravity(Gravity.BOTTOM)
-                        .setText(text);
-                builder.show();
+                Toast toast=Toast.makeText(context,text,Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0,0);
+                toast.show();
                 return false;
             }
         });
